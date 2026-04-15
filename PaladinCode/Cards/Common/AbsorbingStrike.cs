bf9e01091c9fbfc6f9a182cd36c9557faaf8df0e
Vini009hd <vincent.riegler@gmail.com>
2026-04-15 02:24:13 +0200

    Add Absorbing Strike and Image
    # Conflicts:
    #	Paladin.csproj
    #	Paladin/localization/eng/cards.json

---
+++ PaladinCode/Cards/Common/AbsorbingStrike.cs
@@ -0,0 +1,36 @@
+﻿using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Commands.Builders;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Models.Powers;
+
+
+#nullable enable
+namespace Paladin.PaladinCode.Cards.Common;
+
+public sealed class AbsorbingStrike : PaladinCard
+{
+    public AbsorbingStrike()
+        : base(1, CardType.Attack, CardRarity.Common, TargetType.AnyEnemy)
+    {
+        WithDamage(7);
+        WithPower<PlatingPower>(2);
+        WithTags(CardTag.Strike);
+    }
+    
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        AbsorbingStrike card = this;
+        ArgumentNullException.ThrowIfNull((object) cardPlay.Target, "cardPlay.Target");
+        AttackCommand attackCommand = await DamageCmd.Attack(card.DynamicVars.Damage.BaseValue).FromCard((CardModel) card).Targeting(cardPlay.Target).WithHitFx("vfx/vfx_attack_slash").Execute(choiceContext);
+        PlatingPower platingPower = await PowerCmd.Apply<PlatingPower>(card.Owner.Creature, card.DynamicVars["PlatingPower"].BaseValue, card.Owner.Creature, (CardModel) card);
+    }
+
+    protected override void OnUpgrade()
+    {
+        this.DynamicVars.Damage.UpgradeValueBy(2M);
+        this.DynamicVars["PlatingPower"].UpgradeValueBy(1M);
+    } 
+}
