870c8db9fdbc7764437f89b2d32afd1b48ae94cd
Vini009hd <vincent.riegler@gmail.com>
2026-04-15 16:18:16 +0200

    Add Donning Plate and Shield Break with Image

---
+++ PaladinCode/Cards/Common/ShieldBreak.cs
@@ -0,0 +1,35 @@
+﻿using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Commands.Builders;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Models.Powers;
+
+namespace Paladin.PaladinCode.Cards.Common;
+
+public sealed class ShieldBreak : PaladinCard
+{
+    public ShieldBreak()
+        : base(0, CardType.Attack, CardRarity.Common, TargetType.AnyEnemy)
+    {
+        WithDamage(7);
+    }
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        ShieldBreak cardSource = this;
+        ArgumentNullException.ThrowIfNull((object)cardPlay.Target, "cardPlay.Target");
+        await CreatureCmd.TriggerAnim(cardSource.Owner.Creature, "Cast", cardSource.Owner.Character.CastAnimDelay);
+        VfxCmd.PlayOnCreatureCenter(cardSource.Owner.Creature, "vfx/vfx_flying_slash");
+        await CreatureCmd.LoseBlock(cardPlay.Target, (Decimal)cardPlay.Target.Block);
+        AttackCommand attackCommand = await DamageCmd.Attack(cardSource.DynamicVars.Damage.BaseValue)
+            .FromCard((CardModel)cardSource).Targeting(cardPlay.Target).WithHitFx("vfx/vfx_attack_slash")
+            .Execute(choiceContext);
+    }
+    protected override void OnUpgrade()
+    {
+        this.DynamicVars.Damage.UpgradeValueBy(3M);
+    } 
+}
+
---
+++ PaladinCode/Cards/Uncommon/DonningPlate.cs
@@ -0,0 +1,35 @@
+﻿using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Commands.Builders;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Models.Powers;
+
+namespace Paladin.PaladinCode.Cards.Uncommon;
+
+
+public sealed class DonningPlate : PaladinCard
+{
+    public DonningPlate()
+        : base(1, CardType.Power, CardRarity.Uncommon, TargetType.Self)
+
+    {
+        WithPower<PlatingPower>(6);
+        WithPower<VulnerablePower>(2);
+    }
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        DonningPlate cardSource = this;
+        await CreatureCmd.TriggerAnim(cardSource.Owner.Creature, "Cast", cardSource.Owner.Character.CastAnimDelay); 
+        PlatingPower? platingPower = await PowerCmd.Apply<PlatingPower>(cardSource.Owner.Creature, cardSource.DynamicVars["PlatingPower"].BaseValue, cardSource.Owner.Creature, (CardModel) cardSource); 
+        Task<VulnerablePower?> vulnerablePower = PowerCmd.Apply<VulnerablePower>(cardSource.Owner.Creature, cardSource.DynamicVars["VulnerablePower"].BaseValue, cardSource.Owner.Creature, (CardModel) cardSource);
+    }
+    
+    protected override void OnUpgrade()
+    {
+        DynamicVars["PlatingPower"].UpgradeValueBy(1m);
+        DynamicVars["VulnerablePower"].UpgradeValueBy(-1m);
+    }
+    
+}
