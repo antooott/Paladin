ef199fd2fe103e09862e1589f50621b59d17ec79
antooott <amds.coelho4@gmail.com>
2026-04-15 02:32:34 +0200

    Add Spell Slot Mechanic to Searing Smite

--- PaladinCode/Cards/Common/SearingSmite.cs
+++ PaladinCode/Cards/Common/SearingSmite.cs
@@ -1,4 +1,5 @@
 using BaseLib.Abstracts;
+using BaseLib.Extensions;
 using BaseLib.Utils;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
@@ -22,6 +23,7 @@ public sealed class SearingSmite : PaladinCard
     {
         WithPower<VigorPower>(6);
         WithPower<SearingSmitePower>(3);
+        WithPower<SpellSlotPower>(-this.CanonicalEnergyCost);
     }
 
     protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay play)
@@ -29,11 +31,25 @@ public sealed class SearingSmite : PaladinCard
         await CommonActions.ApplySelf<VigorPower>(this, DynamicVars["VigorPower"].BaseValue);
         await Cmd.Wait(0.1f);
         await CommonActions.ApplySelf<SearingSmitePower>(this, DynamicVars["SearingSmitePower"].BaseValue);
+        await CommonActions.ApplySelf<SpellSlotPower>(this, DynamicVars["SpellSlotPower"].BaseValue);
     }
 
     protected override void OnUpgrade()
     {
         DynamicVars["VigorPower"].UpgradeValueBy(2m);
         DynamicVars["SearingSmitePower"].UpgradeValueBy(1m);
+    }
+    
+    protected override bool IsPlayable
+    {
+        get
+        {
+            SearingSmite searingSmite = this;
+            if (searingSmite.Owner.HasPower<SpellSlotPower>() &&  searingSmite.Owner.Creature.GetPowerAmount<SpellSlotPower>() >= searingSmite.CanonicalEnergyCost)
+            {
+                return true;
+            }
+            return false;
+        }
     }
 }
