a2ae0e317e49b3b1f5f742715da8a6a7c7c53c6d
Vini009hd <vincent.riegler@gmail.com>
2026-04-16 22:21:54 +0200

    Add Wide Swing and Image

---
+++ PaladinCode/Cards/Uncommon/WideSwing.cs
@@ -0,0 +1,28 @@
+﻿using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Commands.Builders;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Models;
+
+namespace Paladin.PaladinCode.Cards.Uncommon;
+
+public sealed class WideSwing : PaladinCard
+{
+    public WideSwing()
+        : base(2, CardType.Attack, CardRarity.Uncommon, TargetType.AnyEnemy)
+    {
+        WithDamage(20);
+    }
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay play)
+    {
+        WideSwing card = this;
+        ArgumentNullException.ThrowIfNull((object) play.Target, "cardPlay.Target");
+        await DamageCmd.Attack(card.DynamicVars.Damage.BaseValue).FromCard((PaladinCard) card).Targeting(play.Target)
+            .WithHitFx("vfx/vfx_attack_blunt", tmpSfx: "blunt_attack.mp3").Execute(choiceContext);
+    }
+    protected override void OnUpgrade()
+    {
+        this.DynamicVars.Damage.UpgradeValueBy(6M);
+    } 
+}
